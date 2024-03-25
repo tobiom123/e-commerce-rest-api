ee421dd5607887516590d0bc95552d02b1c4890d
cf_tomotosho <tomotosho@crossflowpayments.co.uk>
2024-03-25 09:58:44 +0000

    Output secret through reflection instance

--- reflect.php
+++ reflect.php
@@ -3,3 +3,8 @@
 final class ReflectionTest {
     private $mySecret = 'I have 99 problems. This isn\'t one of them.';
 }
+
+
+$reflection = new ReflectionClass('ReflectionTest');
+$mySecretProperty = $reflection->getProperty('mySecret');
+echo $mySecretProperty->getValue($reflection->newInstance());
